3ae1710be0890d5be5ca95a328e48059eab5026f
Cosmin Costeniuc <110791830+C0SMlN@users.noreply.github.com>
2024-10-15 17:30:27 +0300

    ex laborator 3

---
+++ Lab3/laboratorul3.py
@@ -0,0 +1,34 @@
+from pgmpy.models import BayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+
+model = BayesianNetwork([('S', 'O'), ('S', 'L'), ('S', 'M'), ('L', 'M')])
+
+cpd_O = TabularCPD(variable='O', variable_card=2,
+                          values=[[0.1, 0.3], #S=0
+                                  [0.9, 0.7]] #S=1
+                   , evidence=['S'], evidence_card=[2])
+
+cpd_L = TabularCPD(variable='L', variable_card=2,
+                          values=[[0.3, 0.2], #S=0
+                                  [0.7, 0.8]] #S=1
+                   , evidence=['S'], evidence_card=[2])
+
+cpd_M = TabularCPD(variable='M', variable_card=2,
+                          values=[
+                              [0.8, 0.4, 0.5, 0.1],  # P(D = 0 | B, T)
+                              [0.2, 0.6, 0.5, 0.9]  # P(D = 1 | B, T)
+                          ], evidence=['S', 'L'], evidence_card=[2, 2])
+
+cpd_S = TabularCPD(variable='S', variable_card=2, values=[[0.6], [0.4]])
+
+
+
+model.add_cpds(cpd_S, cpd_M, cpd_L, cpd_O)
+
+inference = VariableElimination(model)
+
+probabilitate = inference.query(variables=['S'], evidence = {'O': 1, 'L': 1, 'M': 1})
+print(probabilitate)
+
+prob_spam = probabilitate[1]
